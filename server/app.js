31ea27398f34d4919ba390bb5edca12a5a16e20f
이석훈 <cqe9933@naver.com>
2023-04-26 21:19:40 +0900

    feat: 배포시 render 오류 해결

--- server/app.js
+++ server/app.js
@@ -51,7 +51,6 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  res.render("error");
 });
 
 const server = app.listen(app.get("port"), () => {
