de39840edafe488194e6599b9c6da759108cb090
이석훈 <cqe9933@naver.com>
2023-05-02 14:27:24 +0900

    feat: redis 유저 room enter out 구현

--- server/socket.js
+++ server/socket.js
@@ -5,37 +5,61 @@ const redis = require("redis");
 const client = redis.createClient({
   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
 });
-client.on('connect', () => {
-  console.info('Redis connected!');
+
+client.on("connect", () => {
+  console.info("Redis connected!");
 });
 
 //방 입장시 유저 정보, 유저 소켓 아이디에 따른 실제 유저 아이디를 Redis에 저장
 const saveUser = async (data, socket) => {
-  console.log(data.roomId)
-await client.set(String(socket.id), String(data.user));
- await client.lpush(String(data.roomId), String(socket.id));
-
-
-
+  await client.set(String(socket.id), String(data.user));
+  await client.set(String(data.user), String(data.roomId));
+  await client.lpush(String(data.roomId), String(socket.id));
 };
 
-//소켓 정보로 유저 이름 정보 조회
-const findUserName = async(socket)=>{
-  let thisUserName = null;
+const findUserRoom = async (key) => {
+  return new Promise((resolve, reject) => {
+    // GET 명령어 실행
+    client.get(key, (err, value) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(value);
+      }
+    });
+  });
+};
 
- const a = await client.get(String(socket.id),
-  (err,rp)=>{
-    console.log("@@@@@@@@@@@@@")
-    console.log(rp)
-  }
-  
- );
- a
-
-  return thisUserName
+function getValue(key) {
+  return new Promise((resolve, reject) => {
+    // GET 명령어 실행
+    client.get(key, (err, value) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(value);
+      }
+    });
+  });
+}
+//유저가 나온 방의 인원수
+function getUserNum(key) {
+  return new Promise((resolve, reject) => {
+    client.llen(key, (err, value) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(value);
+      }
+    });
+  });
 }
 
-
+//소켓 정보로 유저 이름 정보 조회
+const findUserName = async (socket) => {
+  const value = await getValue(socket.id);
+  return value;
+};
 
 module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, {
@@ -58,49 +82,35 @@ module.exports = (server, app, sessionMiddleware) => {
 
   chat.on("connection", (socket) => {
     console.log("chat 네임스페이스에 접속");
+    let nowSocket = socket;
 
     socket.on("join", (data) => {
       console.log("채팅방에 입장 체크");
       console.log("새 방을 만듦");
-
       saveUser(data, socket);
       socket.join(data.roomId);
-
     });
 
     socket.on("disconnect", async () => {
       console.log("chat 네임스페이스 접속 해제");
+      console.log(nowSocket.id);
+      let thisUserName = await findUserName(nowSocket);
+      if (thisUserName) {
+        let thisUserRoom = await findUserRoom(thisUserName);
 
-      let thisUserName = await findUserName(socket)
- 
-      await client.del(`${socket.id}`)
-      console.log("########################")
-      console.log(thisUserName)
-       const a =  await client.scan("0", 'MATCH', thisUserName);
-       console.log(a)
-      for (let k in roomsUsers) {
-        if (roomsUsers[k][socket.id]) {
-          roomId = k;
-        }
-      }
-      let userCount;
-      if (roomId) {
-        userCount = Object.keys(roomsUsers[roomId]).length;
-      }
+        await client.del(`${nowSocket.id}`);
+        await client.del(`${thisUserName}`);
+        await client.lrem(`${thisUserRoom}`, 0, `${nowSocket.id}`);
 
-      if (userCount) {
-        if (userCount === 1) {
+        const roomLen = await getUserNum(`${thisUserRoom}`);
+        if (Number(roomLen) === 0) {
           // 유저가 0명이면 방 삭제
-          delete roomsUsers[roomId];
-          await removeRoom(roomId); // 컨트롤러 대신 서비스를 사용
-          room.emit("removeRoom", roomId);
-          client.del(roomId);
-          console.log("방 제거 요청 성공");
+          await removeRoom(thisUserRoom); // 컨트롤러 대신 서비스를 사용
+          room.emit("removeRoom", thisUserRoom);
         } else {
-          delete roomsUsers[roomId][socket.id];
-          socket.to(roomId).emit("exit", {
-            user: roomsUsers[roomId],
-            chat: `${roomInfo.user}님이 퇴장하셨습니다.`,
+          socket.to(thisUserRoom).emit("exit", {
+            user: `${thisUserName}`,
+            chat: `${thisUserName}님이 퇴장하셨습니다.`,
           });
         }
       }
