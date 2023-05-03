3f3e2e20b0247c20d2f77d228ad616d1c0221f1b
yeahzizi <newyeahzizi@gmail.com>
2023-05-03 16:59:12 +0900

    feat: 페이 모달 띄우기

--- gemini-front/src/components/profile/pay/modal/PayModal.tsx
+++ gemini-front/src/components/profile/pay/modal/PayModal.tsx
@@ -1,12 +1,26 @@
 import React, { ChangeEvent, useState } from "react";
 import axios from "axios";
+import PayModalBackground from "../../../../assets/img/PayModalBackground.png";
+
+//styled-components
+import {
+  ModalBackground,
+  ModalContainer,
+  Overlay,
+  ModalForm,
+} from "./PayModalstyle";
 
 declare const window: typeof globalThis & {
   IMP: any;
 };
 
-const PayModal = () => {
+interface Props {
+  onClose: () => void;
+}
+
+const PayModal: React.FC<Props> = ({ onClose }) => {
   const [payInput, setPayInput] = useState<number>(0);
+
   // 숫자 입력 input
   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     const currentInput = e.target.value;
@@ -21,7 +35,8 @@ const PayModal = () => {
     const data = {
       pg: "kakaopay",
       pay_method: "card",
-      merchant_uid: "57008833-33004",
+      // merchant_uid가 결제마다 꼭 달라야 함
+      merchant_uid: "570088sfa33004",
       name: "별 구매하기",
       amount: 1000,
     };
@@ -32,7 +47,7 @@ const PayModal = () => {
   const callback = (res: any) => {
     if (res.success) {
       alert("별 구매가 완료되었습니다.");
-      // axios.patch("").then().catch();
+      // axios.post("").then().catch();
     } else {
       alert("다시 한 번 시도하여 주십시오.");
       console.log(res);
@@ -41,7 +56,17 @@ const PayModal = () => {
 
   return (
     <>
-      <button onClick={onClickPayment}>별 구매하자</button>
+      <Overlay onClick={onClose} aria-hidden>
+        <div aria-hidden onClick={(e) => e.stopPropagation()}>
+          <ModalContainer>
+            <ModalForm>
+              <button onClick={onClickPayment}>구매하기</button>
+            </ModalForm>
+
+            <ModalBackground src={PayModalBackground} alt="modal-background" />
+          </ModalContainer>
+        </div>
+      </Overlay>
     </>
   );
 };
--- gemini-front/src/components/profile/pay/modal/PayModalstyle.ts
+++ gemini-front/src/components/profile/pay/modal/PayModalstyle.ts
@@ -1,1 +1,46 @@
 import styled from "styled-components";
+
+export const Overlay = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 9999;
+`;
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 33%;
+  height: 37.5%;
+  z-index: 1;
+`;
+
+export const ModalBackground = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 50;
+  opacity: 0.7;
+`;
+
+export const ModalForm = styled.div`
+  position: absolute;
+  top: 11.5%;
+  left: 11.5%;
+  width: 77%;
+  height: 77%;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+export const PayButton = styled.button``;
