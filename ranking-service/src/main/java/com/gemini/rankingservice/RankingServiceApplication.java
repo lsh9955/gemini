d5b5a3c2c5c6af7c56e39ad279a4d7ec7f48c338
bshello25 <bshello25@gmail.com>
2023-05-04 17:49:54 +0900

    ranking 준비

--- ranking-service/src/main/java/com/gemini/rankingservice/RankingServiceApplication.java
+++ ranking-service/src/main/java/com/gemini/rankingservice/RankingServiceApplication.java
@@ -1,9 +1,11 @@
 package com.gemini.rankingservice;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class RankingServiceApplication {
 
 	public static void main(String[] args) {
---
+++ ranking-service/src/main/java/com/gemini/rankingservice/entity/Gallery.java
@@ -0,0 +1,17 @@
+package com.gemini.rankingservice.entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "gallery")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Gallery {
+
+
+
+
+}
