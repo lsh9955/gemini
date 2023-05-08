5e83cacbca98562abaa50d250336316f114e5299
CessnaJ <eon8748@gmail.com>
2023-05-08 10:35:46 +0900

    Chore: save

--- gemini-front/src/pages/main/Main.tsx
+++ gemini-front/src/pages/main/Main.tsx
@@ -1,8 +1,8 @@
 // import LoginInput from '../../Components/Auth/LoginInput';
 // import GoBackPage from '../../Components/Menu/goBackPage';
 // import { Head, BannerLine, Title } from '../../styles/Menu/NavStyle';
-import React, { FC } from "react";
-// import { useHistory } from "react-router-dom";
+import React, { FC, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import {
   LeftComponent,
   MainWrapper,
@@ -13,16 +13,16 @@ import AnimatedArrow from "../../components/main/AnimatedArrow";
 import Gallery from "../../components/main/Gallery";
 
 const Main: FC = () => {
-  // const history = useHistory();
+  const history = useHistory();
   const accessToken = window.localStorage.getItem("accessToken");
   console.log(`accessToken:${accessToken}`);
 
-  // useEffect(() => {
-  //   if (!accessToken) {
-  //     alert("로그인이 필요합니다. 로그인페이지로 이동합니다.");
-  //     history.push("/loginPage");
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (!accessToken) {
+      alert("로그인이 필요합니다. 로그인페이지로 이동합니다.");
+      history.push("/loginPage");
+    }
+  }, []);
 
   return (
     <>
