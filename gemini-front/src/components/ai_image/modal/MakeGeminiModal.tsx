882751e433ec0994fdaf6d08597917d24b5a759f
yeahzizi <newyeahzizi@gmail.com>
2023-05-10 23:27:07 +0900

    feat: 이미지 생성 후 modal 이동 구현

--- gemini-front/src/components/ai_image/modal/MakeGeminiModal.tsx
+++ gemini-front/src/components/ai_image/modal/MakeGeminiModal.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import CompleteAiImage from "../../../assets/img/CompleteAiImage.png";
 
 // styled-components
@@ -9,28 +10,62 @@ import {
   ModalBackground,
   ConfirmButton,
   CancelButton,
+  Interval,
+  ModalTitle,
+  First,
 } from "./ModalStyle";
+import NeedStarModal from "../modal/NeedStarModal";
+import SuccessGeminiModal from "../modal/SuccessGeminiModal";
 
-interface Props {
+export interface Props {
   onClose: () => void;
 }
 
+type UserState = {
+  user: {
+    star: number;
+  };
+};
+
 const MakeGeminiModal: React.FC<Props> = ({ onClose }) => {
+  //   const star = useSelector((state: UserState) => state.user.star);
+  const star: number = 0;
+  const [currentModal, setCurrentModal] = useState<React.ReactNode>("");
+
+  const handleConfirmClick = () => {
+    if (star === 0) {
+      setCurrentModal(<NeedStarModal onClose={onClose} />);
+      //   onClose();
+    } else {
+      setCurrentModal(<SuccessGeminiModal onClose={onClose} />);
+      //   onClose();
+    }
+  };
+
   return (
     <>
-      <Overlay onClick={onClose} aria-hidden>
-        <div aria-hidden onClick={(e) => e.stopPropagation()}>
-          <ModalContainer>
-            <ModalForm>
-              <div>생성 시 별 1개가 차감됩니다</div>
-              <div>결제하시겠어요?</div>
-              <ConfirmButton>확인</ConfirmButton>
-              <CancelButton>취소</CancelButton>
-            </ModalForm>
-            <ModalBackground src={CompleteAiImage} alt="modal-background" />
-          </ModalContainer>
-        </div>
-      </Overlay>
+      {!currentModal && (
+        <Overlay onClick={onClose} aria-hidden>
+          <div aria-hidden onClick={(e) => e.stopPropagation()}>
+            <ModalContainer>
+              <ModalForm>
+                <ModalTitle>
+                  <First>생성 시 별 1개가 차감됩니다</First>
+                  <div>결제하시겠어요?</div>
+                </ModalTitle>
+                <Interval>
+                  <ConfirmButton onClick={handleConfirmClick}>
+                    확인
+                  </ConfirmButton>
+                  <CancelButton onClick={onClose}>취소</CancelButton>
+                </Interval>
+              </ModalForm>
+              <ModalBackground src={CompleteAiImage} alt="modal-background" />
+            </ModalContainer>
+          </div>
+        </Overlay>
+      )}
+      {currentModal}
     </>
   );
 };
