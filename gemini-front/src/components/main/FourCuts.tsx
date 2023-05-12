ad68086dce4708cf30508093d0001bc2a3742f37
yeahzizi <newyeahzizi@gmail.com>
2023-05-12 16:23:00 +0900

    chore: front-dev pull 받기 전

--- gemini-front/src/components/main/FourCuts.tsx
+++ gemini-front/src/components/main/FourCuts.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-interface Props {
-  onClose: () => void;
-}
-
-const FourCuts: React.FC<Props> = ({ onClose }) => {
+const FourCuts: React.FC = () => {
   return (
     <>
       <div />
