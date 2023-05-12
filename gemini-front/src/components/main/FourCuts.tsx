552313c72eac1495609020ace6a9723f6a72a6c8
yeahzizi <newyeahzizi@gmail.com>
2023-05-12 16:18:04 +0900

    feat:인생네컷 디렉토리

---
+++ gemini-front/src/components/main/FourCuts.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  onClose: () => void;
+}
+
+const FourCuts: React.FC<Props> = ({ onClose }) => {
+  return (
+    <>
+      <div />
+    </>
+  );
+};
+
+export default FourCuts;
