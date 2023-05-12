d3e1c43eaef0e6945de0be747dc77043723a952f
yeahzizi <newyeahzizi@gmail.com>
2023-05-12 10:55:50 +0900

    feat: 좋아요 알람 구현

--- user-service/src/main/java/com/gemini/userservice/service/AlarmService.java
+++ user-service/src/main/java/com/gemini/userservice/service/AlarmService.java
@@ -1,6 +1,7 @@
 package com.gemini.userservice.service;
 
 import com.gemini.userservice.dto.Alarm.FollowAlarmDto;
+import com.gemini.userservice.dto.Alarm.LikeAlarmDto;
 import com.gemini.userservice.dto.response.ResponseAlarmDto;
 import com.gemini.userservice.repository.FollowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,7 @@ import java.util.List;
 
 public interface AlarmService {
     ResponseAlarmDto createFollowAlarm(String username, FollowAlarmDto alarmDto, SseEmitter emitter);
+
+
+    ResponseAlarmDto createLikeAlarm(String username, LikeAlarmDto likeAlarmDto, SseEmitter emitter);
 }
