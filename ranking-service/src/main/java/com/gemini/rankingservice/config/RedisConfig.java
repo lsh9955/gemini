8c3e6a0eb6636f75d79daa963f9fa502809cb4f9
bshello25 <bshello25@gmail.com>
2023-05-13 22:24:47 +0900

    reward emotion 추가

--- ranking-service/src/main/java/com/gemini/rankingservice/config/RedisConfig.java
+++ ranking-service/src/main/java/com/gemini/rankingservice/config/RedisConfig.java
@@ -3,16 +3,19 @@ package com.gemini.rankingservice.config;
 import com.gemini.rankingservice.entity.Gallery;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@ComponentScan(basePackages = {"com.gemini.rankingservice.config"})
+@Order(1)
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
---
+++ ranking-service/src/main/java/com/gemini/rankingservice/repository/DailyRepository.java
@@ -0,0 +1,8 @@
+package com.gemini.rankingservice.repository;
+
+import com.gemini.rankingservice.entity.Daily;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DailyRepository extends CrudRepository<Daily, Long> {
+
+}
