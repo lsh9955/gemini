1b868a4412e7ecd75037927095a58272efc45a9c
yeahzizi <newyeahzizi@gmail.com>
2023-05-14 17:29:40 +0900

    feat: alarm 프론트 로직

--- gemini-front/src/components/common/alarm/AlarmModal.tsx
+++ gemini-front/src/components/common/alarm/AlarmModal.tsx
@@ -8,21 +8,19 @@ import {
   AlarmContentWrapper,
 } from "./AlarmModalStyle";
 
-type Alarm = {
-  alarmId: number;
-  memo: string;
-  checked: boolean;
-  category: number;
-};
+// type Alarm = {
+//   alarmId: number;
+//   memo: string;
+//   checked: boolean;
+//   category: number;
+// };
 
 interface Props {
   onClose: () => void;
-  alarmList: Alarm[];
+  alarmList: any;
 }
 
 const AlarmModal: React.FC<Props> = ({ onClose, alarmList }) => {
-  // const alarmList;
-
   // const updateAlarmList = (newAlarm: Alarm) => {
   //   // 알림 id가 이미 리스트에 존재하는 경우에는 업데이트를 하지 않습니다.
   //   if (initialAlarmList.find((alarm) => alarm.alarmId === newAlarm.alarmId)) {
@@ -33,32 +31,32 @@ const AlarmModal: React.FC<Props> = ({ onClose, alarmList }) => {
   // };
   // console.log(alarmList);
   // console.log(alarmList);
-  const alarmMesseges = [
-    {
-      id: 1,
-      content: "Alarm1",
-    },
-    {
-      id: 2,
-      content: "Alarm2",
-    },
-    {
-      id: 3,
-      content: "Alarm3",
-    },
-    {
-      id: 4,
-      content: "Alarm4",
-    },
-    {
-      id: 5,
-      content: "Alarm5",
-    },
-    {
-      id: 6,
-      content: "Alarm6",
-    },
-  ];
+  // const alarmMesseges = [
+  //   {
+  //     id: 1,
+  //     content: "Alarm1",
+  //   },
+  //   {
+  //     id: 2,
+  //     content: "Alarm2",
+  //   },
+  //   {
+  //     id: 3,
+  //     content: "Alarm3",
+  //   },
+  //   {
+  //     id: 4,
+  //     content: "Alarm4",
+  //   },
+  //   {
+  //     id: 5,
+  //     content: "Alarm5",
+  //   },
+  //   {
+  //     id: 6,
+  //     content: "Alarm6",
+  //   },
+  // ];
 
   return (
     <>
@@ -72,12 +70,12 @@ const AlarmModal: React.FC<Props> = ({ onClose, alarmList }) => {
                 overflowY: alarmList.length >= 6 ? "auto" : "visible",
               }}
             >
-              {alarmMesseges.length === 0 ? (
+              {alarmList.length === 0 ? (
                 <NoAlarmContent>받은 알람이 없습니다.</NoAlarmContent>
               ) : (
-                alarmMesseges.map((alarm, idx) => (
-                  <AlarmContent key={alarm.id} idx={alarm.id}>
-                    {alarm.content}
+                alarmList[0].map((alarm: any, idx: any) => (
+                  <AlarmContent key={alarm.alarmId} idx={idx}>
+                    {alarm.memo}
                   </AlarmContent>
                 ))
               )}
