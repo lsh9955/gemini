5e25ed4e06ecc1626f6fd4e749657d81d9b8d625
CessnaJ <eon8748@gmail.com>
2023-05-14 19:08:43 +0900

    Fix: New gemini component size modified

--- gemini-front/src/components/geminiDetail/NewGeminiDetail.tsx
+++ gemini-front/src/components/geminiDetail/NewGeminiDetail.tsx
@@ -22,7 +22,13 @@ import {
   ToggleWrapper,
 } from "./UserGeminiDetail.styles";
 
-import { DescInput, EnrollButton, TextInput } from "./NewGeminiDetail.styles";
+import {
+  DescInput,
+  EnrollButton,
+  NewGeminFlipContainer,
+  NewGeminiFlipContainerWrapper,
+  TextInput,
+} from "./NewGeminiDetail.styles";
 import {
   EditButton,
   EditButtonWrapper,
@@ -52,7 +58,7 @@ const NewGeminiDetail: FC<MyGeminiDetailProps> = ({
   const [likeCount, setLikeCount] = useState<number>(0);
   const [geminiName, setGeminiName] = useState<string>("나나키타 미즈키");
   const [desc, setDesc] = useState<string>(
-    "나나키타 미즈키의 설명을 입력해주세요."
+    "나나키타 미즈키의 설명을 입력해주세요. 데이터를 받아오면 이름과 함께 비워집니다."
   );
   const [geminiImg, setGeminiImg] = useState<string>(
     "https://mygemini.s3.amazonaws.com/gemini/20230508_132357723467_TestUser.png"
@@ -118,57 +124,62 @@ const NewGeminiDetail: FC<MyGeminiDetailProps> = ({
   // 모달 밖을 눌러서 닫는 액션을 취하면 안됨 (버그의 원인. 빈 정보의 제미니가 나옴.)
   return (
     <>
-      <GeminiDetialWrapper>
-        <GeminiDetailImgWrapper backgroundImage={geminiImg}>
-          {/* <LikeNicknameWrapper>
-            <LinkProfileWrapper>
-              <LinkImg></LinkImg>
-            </LinkProfileWrapper>
-            <LikeWrapper>
-              <HeartIcon>❤️</HeartIcon>
-              <LikeCount>{likeCount}개의 좋아요</LikeCount>
-            </LikeWrapper>
-          </LikeNicknameWrapper> */}
-        </GeminiDetailImgWrapper>
-        <GeminiDetialInfoWrapper>
-          <ToggleWrapper>
-            <ToggleText>공개</ToggleText>
-            {/* isPublic의 반대값을 줘야함. BE와 통신할때는 true로 보내는데, toggle로 보여지는 상태는 false */}
-            <ToggleButtonContainer onClick={handleClick} isOn={!isPublic}>
-              <ToggleButtonCircle isOn={!isPublic} />
-            </ToggleButtonContainer>
-            <ToggleText>비공개</ToggleText>
-          </ToggleWrapper>
-          <NameInputWrapper>
-            <FormLabel>이름</FormLabel>
-            <TextInput
-              type="text"
-              id="nickname"
-              value={geminiName}
-              onChange={(e) => setGeminiName(e.target.value)}
-              placeholder="15글자 이하 한글, 영어, 숫자만 입력가능"
-            ></TextInput>
-          </NameInputWrapper>
-          <DescBlockWrapper>
-            <FormLabel>소개</FormLabel>
-            <DescInput>{desc}</DescInput>
-          </DescBlockWrapper>
-          <TagBlockWrapper>
-            <FormLabel>키워드</FormLabel>
-            <TagArea>
-              {tagContents.map((tag, index) => (
-                <Tags key={index}>{tag}</Tags>
-              ))}
-            </TagArea>
-          </TagBlockWrapper>
-          <ButtonWrapper>
-            <EditButtonWrapper>
-              <EnrollButton onClick={contractGemini}>계약하기</EnrollButton>
-              <EnrollButton onClick={closeModal}>놓아주기</EnrollButton>
-            </EditButtonWrapper>
-          </ButtonWrapper>
-        </GeminiDetialInfoWrapper>
-      </GeminiDetialWrapper>
+      <NewGeminiFlipContainerWrapper onClick={closeModal}>
+        <NewGeminFlipContainer
+          isFlipped={false}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <GeminiDetailImgWrapper backgroundImage={geminiImg}>
+            {/* <LikeNicknameWrapper>
+              <LinkProfileWrapper>
+                <LinkImg></LinkImg>
+              </LinkProfileWrapper>
+              <LikeWrapper>
+                <HeartIcon>❤️</HeartIcon>
+                <LikeCount>{likeCount}개의 좋아요</LikeCount>
+              </LikeWrapper>
+            </LikeNicknameWrapper> */}
+          </GeminiDetailImgWrapper>
+          <GeminiDetialInfoWrapper>
+            <ToggleWrapper>
+              <ToggleText>공개</ToggleText>
+              {/* isPublic의 반대값을 줘야함. BE와 통신할때는 true로 보내는데, toggle로 보여지는 상태는 false */}
+              <ToggleButtonContainer onClick={handleClick} isOn={!isPublic}>
+                <ToggleButtonCircle isOn={!isPublic} />
+              </ToggleButtonContainer>
+              <ToggleText>비공개</ToggleText>
+            </ToggleWrapper>
+            <NameInputWrapper>
+              <FormLabel>이름</FormLabel>
+              <TextInput
+                type="text"
+                id="nickname"
+                value={geminiName}
+                onChange={(e) => setGeminiName(e.target.value)}
+                placeholder="15글자 이하 한글, 영어, 숫자만 입력가능"
+              ></TextInput>
+            </NameInputWrapper>
+            <DescBlockWrapper>
+              <FormLabel>소개</FormLabel>
+              <DescInput>{desc}</DescInput>
+            </DescBlockWrapper>
+            <TagBlockWrapper>
+              <FormLabel>키워드</FormLabel>
+              <TagArea>
+                {tagContents.map((tag, index) => (
+                  <Tags key={index}>{tag}</Tags>
+                ))}
+              </TagArea>
+            </TagBlockWrapper>
+            <ButtonWrapper>
+              <EditButtonWrapper>
+                <EnrollButton onClick={contractGemini}>계약하기</EnrollButton>
+                <EnrollButton onClick={closeModal}>놓아주기</EnrollButton>
+              </EditButtonWrapper>
+            </ButtonWrapper>
+          </GeminiDetialInfoWrapper>
+        </NewGeminFlipContainer>
+      </NewGeminiFlipContainerWrapper>
     </>
   );
 };
