16145226bfcc8e901f3c1f70b0aa31492eed7c7f
yeahzizi <newyeahzizi@gmail.com>
2023-05-15 14:28:03 +0900

    chore: axios 변경

--- gemini-front/src/components/common/alarm/AlarmModal.tsx
+++ gemini-front/src/components/common/alarm/AlarmModal.tsx
@@ -10,6 +10,8 @@ import {
   NoAlarmContent,
   AlarmContentWrapper,
 } from "./AlarmModalStyle";
+import UserGeminiDetail from "../../geminiDetail/UserGeminiDetail";
+import axiosInstanceWithAccessToken from "../../../utils/AxiosInstanceWithAccessToken";
 
 // type Alarm = {
 //   alarmId: number;
@@ -31,37 +33,60 @@ const AlarmModal: React.FC<Props> = ({ onClose, alarmList }) => {
 
   const handleAlarmClick = async (alarmId: number, category: number) => {
     // 카테고리에 따라 페이지 이동이나 모달 표시를 다르게 처리합니다.
+    const selectAlarmList = alarmList
+      .slice(-1)[0]
+      .find(
+        (alarm: any) => alarm.alarmId === alarmId && alarm.category === category
+      );
     switch (category) {
       case 1:
         // 팔로우 했을 때
-        history.push("/mypage");
+        history.push(`/userProfile/${selectAlarmList.follower}`);
+        // 알람 삭제 요청 보내기
+        try {
+          await axiosInstanceWithAccessToken.delete(`/alarms/${alarmId}`);
+          // await axios.delete(`/alarms/${alarmId}`, {
+          //   headers: {
+          //     "X-username": "yyj", // 토큰을 사용하는 경우 예시입니다
+          //   },
+          // });
+        } catch (error) {
+          console.error("알람 삭제 실패:", error);
+        }
         break;
 
       case 2:
         // 좋아요 눌렸을 때
-        break;
-      case 3:
-        // 제미니 생성
-        const geminino = alarmList[-1].find(
-          (alarm: any) =>
-            alarm.alarmId === alarmId && alarm.category === category
-        );
-        console.log(geminino);
-        if (geminino) {
-          const geminiDetailModal = (
-            <NewGeminiDetail
+        if (selectAlarmList) {
+          const GeminiDetailModal = (
+            <UserGeminiDetail
               closeModal={() => setCurrentModal("")}
-              selectedImagePk={geminino}
+              selectedImagePk={selectAlarmList.geminiNo}
             />
           );
-          setCurrentModal(geminiDetailModal);
+          setCurrentModal(GeminiDetailModal);
         }
         // 알람 삭제 요청 보내기
         try {
-          await axios.delete(`http://172.30.1.22:8081/alarms/${alarmId}`);
-
-          // 삭제 성공 후 알람 리스트 업데이트 등 필요한 작업 수행
-          // 예를 들어, 알람 리스트에서 해당 알람을 제거하는 작업을 수행할 수 있습니다.
+          await axiosInstanceWithAccessToken.delete(`/alarms/${alarmId}`);
+        } catch (error) {
+          console.error("알람 삭제 실패:", error);
+        }
+        break;
+      case 3:
+        // 제미니 생성
+        if (selectAlarmList) {
+          const NewgeminiDetailModal = (
+            <NewGeminiDetail
+              closeModal={() => setCurrentModal("")}
+              selectedImagePk={selectAlarmList.geminiNo}
+            />
+          );
+          setCurrentModal(NewgeminiDetailModal);
+        }
+        // 알람 삭제 요청 보내기
+        try {
+          await axiosInstanceWithAccessToken.delete(`/alarms/${alarmId}`);
         } catch (error) {
           console.error("알람 삭제 실패:", error);
         }
@@ -84,7 +109,7 @@ const AlarmModal: React.FC<Props> = ({ onClose, alarmList }) => {
                 {alarmList.length === 0 ? (
                   <NoAlarmContent>받은 알람이 없습니다.</NoAlarmContent>
                 ) : (
-                  alarmList[-1].map((alarm: any, idx: any) => (
+                  alarmList.slice(-1)[0].map((alarm: any, idx: any) => (
                     <AlarmContent
                       key={alarm.alarmId}
                       idx={idx}
--- gemini-front/src/components/common/header/Header.tsx
+++ gemini-front/src/components/common/header/Header.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useEffect, useState } from "react";
-import axios from "axios";
+import { useSelector } from "react-redux";
 import { StyledHeader } from "./Header.styles";
 import HeaderLogo from "./HeaderLogo";
 import HeaderTools from "./HeaderTools";
+import { AppStore } from "../../../store/store";
 
 type Alarm = {
   alarmId: number;
@@ -14,10 +15,16 @@ type Alarm = {
 const Header: FC = () => {
   const [alarmList, setAlarmList] = useState<Alarm[]>([]);
 
+  const reduxNickname: any = useSelector(
+    (state: AppStore) => state.user.nickname
+  );
+
+  const accessToken = window.localStorage.getItem("accessToken");
+
   useEffect(() => {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://172.30.1.22:8081/alarms");
-    xhr.setRequestHeader("nickname", "yeji");
+    xhr.open("GET", "https://mygemini.co.kr/alarms");
+    xhr.setRequestHeader("Authorization", `Bearer ${accessToken}`);
     xhr.responseType = "text"; // 텍스트 응답을 받을 수 있도록 설정
     xhr.withCredentials = true;
     xhr.send();
@@ -26,7 +33,7 @@ const Header: FC = () => {
     // xhr.setRequestHeader("X-Access-Token", "your-access-token");
     // xhr.setRequestHeader("X-Nickname", "your-nickname");
 
-    const url = "http://172.30.1.22:8081/alarms?nickname=yeji";
+    const url = `https://mygemini.co.kr/alarms?nickname=${reduxNickname}`;
     const eventSource = new EventSource(url, {
       withCredentials: true,
     });
@@ -39,7 +46,6 @@ const Header: FC = () => {
       xhr.abort();
     };
   }, []);
-  console.log(alarmList);
 
   return (
     <>
