27ba2c4d3aca56fded526a3dcc9ee72eea76309f
yeahzizi <newyeahzizi@gmail.com>
2023-05-16 21:18:39 +0900

    fix: url 변경

--- user-service/src/main/java/com/gemini/userservice/api/OrdersController.java
+++ user-service/src/main/java/com/gemini/userservice/api/OrdersController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/user-service/order")
 public class OrdersController {
 
     @Autowired
