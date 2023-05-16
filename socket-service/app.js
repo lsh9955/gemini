79f76f17b8b4f6fd7da2a746729a98039892d96e
이석훈 <cqe9933@naver.com>
2023-05-16 11:15:34 +0900

    feat:cookiesession추가

--- socket-service/app.js
+++ socket-service/app.js
@@ -64,6 +64,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.cookieSession());
 const indexRouter = require("./routes");
 app.use("/node", indexRouter);
 
