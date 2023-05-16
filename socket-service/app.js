b64941042aabb60adbba555abae3c4483cc5f327
이석훈 <cqe9933@naver.com>
2023-05-16 10:31:00 +0900

    feat:제발

--- socket-service/app.js
+++ socket-service/app.js
@@ -65,7 +65,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 const indexRouter = require("./routes");
-app.use("/node", indexRouter);
+app.use("/node/", indexRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
