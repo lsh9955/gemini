4fb9354de1de7addef280bff0a0587466d87c79a
yeahzizi <newyeahzizi@gmail.com>
2023-05-16 10:09:12 +0900

    feat: background alarm dto

--- user-service/src/main/java/com/gemini/userservice/api/CompleteApiController.java
+++ user-service/src/main/java/com/gemini/userservice/api/CompleteApiController.java
@@ -1,16 +1,23 @@
 package com.gemini.userservice.api;
 
+import com.gemini.userservice.dto.Alarm.BackgroundAlarmDto;
+import com.gemini.userservice.dto.Alarm.GeminiAlarmDto;
+import com.gemini.userservice.dto.BackgroundDto;
 import com.gemini.userservice.dto.ML.RequestCompleteBackgroundDto;
 import com.gemini.userservice.dto.ML.RequestCompleteGeminiDto;
 import com.gemini.userservice.dto.ML.RequestCompletePoseDto;
+import com.gemini.userservice.service.AlarmService;
 import com.gemini.userservice.service.CompleteService;
+import com.gemini.userservice.service.EmitterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
@@ -19,12 +26,37 @@ import java.util.List;
 @RequestMapping("/user-service/complete")
 public class CompleteApiController {
 
+    @Autowired
+    private EmitterService emitterService;
+
+    @Autowired
+    private AlarmService alarmService;
+
+
     private final CompleteService completeService;
 
     @PostMapping("/gemini")
     public ResponseEntity<?> completeGemini(@RequestBody RequestCompleteGeminiDto requestCompleteGeminiDto) {
 
         Long res = completeService.completeGemini(requestCompleteGeminiDto);
+
+        SseEmitter emitter = new SseEmitter();
+        emitterService.addEmitter(emitter);
+
+
+        // res가 null 값이 아니면 알람 생성
+        if(res != null) {
+
+            //알림 메세지를 만들기 위해 GeminiAlarmDto에 넣어준다. => service에서 닉네임을 찾아줄 예정
+            GeminiAlarmDto geminiAlarmDto = new GeminiAlarmDto();
+            geminiAlarmDto.setGeminiNo(res);
+            geminiAlarmDto.setUsername(requestCompleteGeminiDto.getUsername());
+
+            //gemini 생성 알림
+            alarmService.createGeminiAlarm(geminiAlarmDto,emitter);
+
+        }
+
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
@@ -32,6 +64,19 @@ public class CompleteApiController {
     public ResponseEntity<?> completeBackground(@RequestBody RequestCompleteBackgroundDto requestCompleteBackgroundDto) {
 
         String res = completeService.completeBackground(requestCompleteBackgroundDto);
+
+        SseEmitter emitter = new SseEmitter();
+        emitterService.addEmitter(emitter);
+
+        // res가 null 값이 아니면 알람 생성
+        if (res != null) {
+            // 알람 메세지를 만들기 위해 BackgroundAlarmDto에 넣어준다 => service에서 닉네임을 찾자
+            BackgroundAlarmDto backgroundAlarmDto = new BackgroundAlarmDto();
+            backgroundAlarmDto.setImageUrl(res);
+//            backgroundAlarmDto.setUsername(requestCompleteBackgroundDt);
+        }
+
+
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
