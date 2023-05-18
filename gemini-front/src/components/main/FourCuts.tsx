c5f2e7147905c0cabdec39d99c862d47d7f7624e
yeahzizi <newyeahzizi@gmail.com>
2023-05-18 21:13:30 +0900

    feat: alarm error 해결

--- gemini-front/src/components/main/FourCuts.tsx
+++ gemini-front/src/components/main/FourCuts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Adol1by1Dummy from "../../assets/img/Adol1by1Dummy.png";
 
 //styled-components
@@ -8,15 +8,26 @@ import {
   MyGeminiText,
   ContainerWrapper,
 } from "../main/FourCutsStyle";
+import axiosInstanceWithAccessToken from "../../utils/AxiosInstanceWithAccessToken";
 // forcuts 컴포넌트는 transform: "scaleY(-1)" 를 사용해서 다 뒤집어 놓기
+
+interface Image {
+  url: String;
+}
+
 interface FourCutsProps {
   backModal: () => void;
 }
 
 const FourCuts: React.FC<FourCutsProps> = ({ backModal }) => {
+  const [emotionCuts, setEmotionCuts] = useState<Image[]>([]);
   const handleClick = () => {
     backModal(); // 모달 닫기 함수 호출
   };
+  useEffect(() => {
+    axiosInstanceWithAccessToken.get("").then().catch();
+    return;
+  });
   // 나중에 get 받고 map해서 이미지 빼기
   return (
     <ContainerWrapper onClick={handleClick}>
